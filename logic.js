0f82e1effdb3f115c8c21bee67b003ba316292df
Gonzalez <0291152@students.dadeschools.net>
2018-04-06 10:59:05 -0400

    Done with logic

--- logic.js
+++ logic.js
@@ -1,14 +1,16 @@
-var gamesList = ["rainbow six siege", "mario kart", "counter strike"];
+
+
+var gamesList = ["rainbow six siege", "mario kart", "counter strike", "l o z ocarina of time"];
 
 var chosenWord = '';
 
-var letterInChosenWord = [];
+var lettersInChosenWord= [];
 
-var numBlanks = 0;
+var numBlanks = 0;    
 
 var blanksAndSuccesses = [];
 
-var wrongGuesses = []
+var wrongGuesses = [];
 
 var letterGuessed = '';
 
@@ -18,86 +20,79 @@ var lossCounter = 0;
 var numGuesses = 9;
 
 
-
-
-
-
-
-
-function startGame(	) {
+function startGame() {
 
 	numGuesses = 9;
 
 	chosenWord = gamesList[Math.floor(Math.random() * gamesList.length)];
-  
-	letterInChosenWord = chosenWord.split("");
 
-	numBlanks = letterInChosenWord.length;
+	lettersInChosenWord = chosenWord.split("");
+
+	numBlanks = lettersInChosenWord.length;
 
 	console.log(chosenWord);
 
-blanksAndSuccesses * [];
+	blanksAndSuccesses = [];
 
-wrongGuesses * [];
-for (var i =0; i <numBlanks; i++){
-	blanksAndSuccesses.push("_");
+	wrongGuesses = [];
+
+	for (var i = 0; i < numBlanks; i++) {
+		blanksAndSuccesses.push("_");
+	}
+
+	console.log(blanksAndSuccesses);
+
+
+	document.getElementById("guesses-left").innerHTML = numGuesses;
+
+	document.getElementById("word-blanks").innerHTML = blanksAndSuccesses.join(" ");
+
+	document.getElementById("wrong-guesses").innerHTML = wrongGuesses.join(" ");
+
 }
-
-console.log(blanksAndSuccesses)
-
-document.getElementById("guesses-left").innerHTML = numGuesses;
-
-document.getElementById("word-blanks").innerHTML = blanksAndSuccesses.join(" ")
-
-document.getElementById("wrong-guesses").innerHTML = wrongGuesses.join(" ")
-
-}
-
 
 function checkLetters(letter) {
 	var letterInWord = false;
 
-	for (var i=0; i < numBlanks; i++){
-		if (chosenWord[i] === letter){
+	for (var i=0; i < numBlanks; i++) {
+		if (chosenWord[i] === letter) {
 			letterInWord = true;
 		}
 	}
 
 
-
-
-if (LetterInWord){
-	for (var j=0; j < numBlanks; j++) {
-		if (chosenWord[j] === letter) {
-			blanksAndSuccesses[j] = letter;
+	if (letterInWord) {
+		for (var j=0; j < numBlanks; j++) {
+			if (chosenWord[j] === letter) {
+				blanksAndSuccesses[j] = letter;
+			}
 		}
-	}
-    console.log(blanksAndSuccesses);
-}
-else{
-	wrongGuesses.push(letter);
-	numBlanks--;
+		console.log(blanksAndSuccesses);
+	} 
 
+	else {
+		wrongGuesses.push(letter);
+		numGuesses--;
 	}
 }
-
 
 function roundComplete() {
-	console.log("WinCount: " + winCounter + " | LossCount:" + lossCounter + " | NumGuesses" + numGuesses);
+	console.log("WinCount: " + winCounter + " | LossCount: " + lossCounter + 
+		" | NumGuesses: " + numGuesses);
 
-	document.getElementById("guesses-left").innerHTML = numGuesses
-	document.getElementById("word-blanks").innerHTML = blanksAndSuccesses.join(" ")
-	document.getElementById("wrong-guesses").innerHTML = wrongGuesses.join(" ")
+	document.getElementById("guesses-left").innerHTML = numGuesses;
+	document.getElementById("word-blanks").innerHTML = blanksAndSuccesses.join(" ");
+	document.getElementById("wrong-guesses").innerHTML = wrongGuesses.join(" ");
 
-	if(letterInChosenWord.toString() === blanksAndSuccesses.toString()){
+	if (lettersInChosenWord.toString() === blanksAndSuccesses.toString()) {
 		winCounter++;
 		alert("You win!");
 
-		document.getElementById("win-counter").innerHTML = winCounter
+		document.getElementById("win-counter").innerHTML = winCounter;
 
 		startGame();
-
 	}
+
 	else if (numGuesses === 0) {
 		lossCounter++;
 		alert("You lose");
@@ -106,24 +101,12 @@ function roundComplete() {
 
 		startGame();
 	}
-
-
-
 }
 
-
-
 startGame();
-
-
-
-
-
-
-
 
 document.onkeyup = function(event) {
 	var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
 	checkLetters(letterGuessed);
 	roundComplete();
-}
+};
