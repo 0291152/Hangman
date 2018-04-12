762d786db09ae12c5f8f347a3b7ac3064c9e5247
Gonzalez <0291152@students.dadeschools.net>
2018-04-12 11:12:23 -0400

    done with all the notes and code, COMPLETE

--- logic.js
+++ logic.js
@@ -1,52 +1,71 @@
 
-
+//Array of words (all lowercase)
 var gamesList = ["rainbow six siege", "mario kart", "counter strike", "l o z ocarina of time", "the sims","super mario world", "street fighter", "doom", "pokemon", "star fox", "metal gear solid", "fortnite"];
 
+//Computer selected solution will be here
 var chosenWord = '';
 
+
+// will break the solution in to individual letters to be storec in array
 var lettersInChosenWord= [];
 
+//number of blanks we show based on the solution
 var numBlanks = 0;    
 
+// holds a mix of blanks and solved letters
 var blanksAndSuccesses = [];
 
+// holds all the wrong guesses 
 var wrongGuesses = [];
 
+// holds all the letters guessed 
 var letterGuessed = '';
 
-
+//GameCounter
 var winCounter = 0;
 var lossCounter = 0;
 var numGuesses = 9;
 
-
+// this is how we 
 function startGame() {
 
+	// resets the guesses back to zero
 	numGuesses = 9;
 
+	// Solution randomly from word list
 	chosenWord = gamesList[Math.floor(Math.random() * gamesList.length)];
 
+	// breaks the word into individual letters
 	lettersInChosenWord = chosenWord.split("");
 
+	// counts the number of letters in the word
 	numBlanks = lettersInChosenWord.length;
 
+	// here we print the solution in the console (for testing purposes)
 	console.log(chosenWord);
 
+	// here we reset the guess and success array at each round
 	blanksAndSuccesses = [];
 
+	// here we reset the wrong guesses from the previous round
 	wrongGuesses = [];
 
+	// here we fill up the blanksAndSuccesses list with the appropriate number of blanks
+	// which is based on the number of letters
 	for (var i = 0; i < numBlanks; i++) {
 		blanksAndSuccesses.push("_");
-	}
+	} 
 
+	// print the initial blanks in the console
 	console.log(blanksAndSuccesses);
 
-
+	// reprints the gueeses left in the console 
 	document.getElementById("guesses-left").innerHTML = numGuesses;
 
+	// prints the blanks at the begining of each round in the HTML
 	document.getElementById("word-blanks").innerHTML = blanksAndSuccesses.join(" ");
 
+	// clears wrong guesses from previous round
 	document.getElementById("wrong-guesses").innerHTML = wrongGuesses.join(" ");
 
 }
